ebd6f1489c83707eefa000cb1d76a3e43f4d582b
Danilo Namitala <danilonamitala@yahoo.com.br>
2023-11-06 23:40:29 -0300

    database

--- web/models.py
+++ web/models.py
@@ -1,3 +1,21 @@
 from django.db import models
 
-# Create your models here.
+class User(models.Model):
+    cpf = models.CharField(max_length=11)
+    name = models.CharField(max_length=50)
+    email = models.CharField(max_length=50)
+    password = models.CharField(max_length=16)
+
+class Round(models.Model):
+    date = models.DateTimeField("data sorteio")
+    valor = models.FloatField("valor")
+
+class Bet(models.Model):
+    user  = models.ForeignKey(User, on_delete=models.CASCADE)
+    round = models.ForeignKey(Round, on_delete=models.CASCADE)
+    group = models.IntegerField("grupo apostado")
+    value = models.IntegerField("valor")
+
+class Result(models.Model):
+    round  = models.ForeignKey(Round, on_delete=models.CASCADE)
+    number = models.IntegerField("resultado")
